a49d2614048a74c7134aa4813a715f45d8a43f9b
alfaridzi <alfaridzi48@gmail.com>
2018-06-27 13:05:13 +0700

    add socialite

--- config/services.php
+++ config/services.php
@@ -34,5 +34,16 @@ return [
         'key'    => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
+    'google' => [
+        'client_id' => env('GOOGLE_KEY'),
+        'client_secret' => env('GOOGLE_SECRET'),
+        'redirect' => env('GOOGLE_CALLBACK_URL'),
+    ],
+
+    'facebook' => [
+        'client_id' => env('FACEBOOK_KEY'),
+        'client_secret' => env('FACEBOOK_SECRET'),
+        'redirect' => env('FACEBOOK_CALLBACK_URL'),
+    ],
 
 ];
